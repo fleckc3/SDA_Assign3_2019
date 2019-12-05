2d2f39ec4a3d701e4aa2c86aee54bb4abb2e2aa5
Colin Fleck <fleckc3@mail.dcu.ie>
2019-12-05 17:57:49 +0000

    added the welcome fragment .java file and the layout xml file associated with it

---
+++ app/src/main/java/com/example/sdaassign32019johndoe/WelcomeTab.java
@@ -0,0 +1,32 @@
+package com.example.sdaassign32019johndoe;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WelcomeTab extends Fragment {
+
+private static final String TAG = "WelcomeTab";
+    public WelcomeTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+       View view = inflater.inflate(R.layout.fragment_welcome_tab, container, false);
+       return view;
+    }
+
+}
