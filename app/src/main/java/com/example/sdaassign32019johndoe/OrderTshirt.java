9d315c7e9c83b143169cd51b1b6c65e3b742a991
Colin Fleck <fleckc3@mail.dcu.ie>
2019-12-10 22:42:31 +0000

    capture image and save it to external storage feature. made name a required field

--- app/src/main/java/com/example/sdaassign32019johndoe/OrderTshirt.java
+++ app/src/main/java/com/example/sdaassign32019johndoe/OrderTshirt.java
@@ -1,8 +1,14 @@
 package com.example.sdaassign32019johndoe;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.InputType;
 import android.util.Log;
@@ -15,9 +21,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 
 /*
@@ -25,7 +41,6 @@ import androidx.fragment.app.Fragment;
  * @author Chris Coughlan 2019
  */
 public class OrderTshirt extends Fragment {
-
 
     public OrderTshirt() {
         // Required empty public constructor
@@ -37,10 +52,15 @@ public class OrderTshirt extends Fragment {
     private EditText mCustomerName;
     private EditText meditDelivery;
     private ImageView mCameraImage;
+    private File image = null;
+
+
+
 
     //static keys
     private static final int REQUEST_TAKE_PHOTO = 2;
     private static final String TAG = "OrderTshirt";
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -54,24 +74,41 @@ public class OrderTshirt extends Fragment {
         meditDelivery.setImeOptions(EditorInfo.IME_ACTION_DONE);
         meditDelivery.setRawInputType(InputType.TYPE_CLASS_TEXT);
 
-        mCameraImage = root.findViewById(R.id.imageView);;
+        mCameraImage = root.findViewById(R.id.imageView);
         Button mSendButton = root.findViewById(R.id.sendButton);
+
+        TextView imageText = root.findViewById(R.id.imageText);
+        String currentImageName = String.valueOf(mCameraImage.getTag());
+        if(currentImageName != "defaultImage"){
+            imageText.setVisibility(View.INVISIBLE);
+        }
+
+
+
 
         //set a listener on the the camera image
         mCameraImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent(v);
+                dispatchTakePictureIntent();
             }
         });
 
         //set a listener to start the email intent.
-        mSendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendEmail(v);
-            }
-        });
+
+        if(mCustomerName != null){
+            mSendButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendEmail(v);
+                }
+            });
+        } else {
+            //toast message asking for users name - ref android dev doc
+            Toast.makeText(getContext(), "Please enter your name!", Toast.LENGTH_SHORT).show();
+
+        }
+
 
 
         //initialise spinner using the integer array
@@ -87,13 +124,125 @@ public class OrderTshirt extends Fragment {
 
     //Take a photo note the view is being passed so we can get context because it is a fragment.
     //update this to save the image so it can be sent via email
-    private void dispatchTakePictureIntent(View v)
-    {
+    private void dispatchTakePictureIntent() {
+        //updated: added save photo functionality and test to make sure camera activity there to handle the intent
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(v.getContext().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createMediaFile(REQUEST_TAKE_PHOTO);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e(TAG, "dispatchTakePictureIntent: Could not create Image file: " + ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+                        "com.example.sdaassign32019johndoe.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
         }
     }
+
+
+
+
+    /***
+     *  method to create a time stamped picture file called by the dispatchTakePictureIntent methods
+     */
+    String currentPhotoPath;
+    private File createMediaFile(int requestCode) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+         if(requestCode == REQUEST_TAKE_PHOTO){
+             String imageFileName = "JPEG_" + timeStamp + "_";
+             String exState = Environment.getExternalStorageState();
+             if(exState.equals(Environment.MEDIA_MOUNTED)){
+                 File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                 image = File.createTempFile(
+                         imageFileName,      /* prefix */
+                         ".jpg",        /* suffix */
+                         storageDir           /* directory */
+                 );
+             } else {
+                 Log.e(TAG, "createImageFile: External Storage is not available");
+             }
+             currentPhotoPath = image.getAbsolutePath();
+             Log.i(TAG, "createImageFile: "+ currentPhotoPath);
+         }
+        return image;
+    }
+
+    public void galleryAddPic()
+    {
+        // Tell the media scanner about the new file so that it is
+        // immediately available to the user.
+        MediaScannerConnection.scanFile(getActivity(),
+                new String[] { image.toString() }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+    }
+
+    private void setPic(String path) {
+
+        // Get the dimensions of the View
+        int targetW = mCameraImage.getWidth();
+        int targetH = mCameraImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        // Get the dimensions of the image
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = 1;
+        // Determine how much to scale down the image
+        if(targetW > 0 || targetH > 0)
+        {
+            scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        }else
+        {
+            Log.e(TAG, "setPic: scaleFactor is 1 continuing ...");
+        }
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        Bitmap mBitMap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        mCameraImage.setImageBitmap(mBitMap);
+    }
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            if(requestCode == REQUEST_TAKE_PHOTO){
+                    if (resultCode == RESULT_OK) {
+                        galleryAddPic();
+                        setPic(currentPhotoPath);
+                        Bitmap bp = BitmapFactory.decodeFile(currentPhotoPath);
+                        mCameraImage.setImageBitmap(bp);
+
+                    }
+
+                } else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(getActivity(), "picture capture fail", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
 
     /*
      * Returns the Email Body Message, update this to handle either collection or delivery
